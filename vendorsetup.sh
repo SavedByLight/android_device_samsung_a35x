f5ae2433bd90f652413efe65ea62064155e03161
Jamie Falconer <mrfluffyoven@gmail.com>
2025-10-16 08:56:28 +0100

    Change target devices for orangefox

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -75,7 +75,7 @@ export OF_UNBIND_SDCARD_F2FS=1
 export FOX_RECOVERY_INSTALL_PARTITION=/dev/block/by-name/recovery
 export FOX_RECOVERY_SYSTEM_PARTITION=/dev/block/mapper/system
 export FOX_RECOVERY_VENDOR_PARTITION=/dev/block/mapper/vendor
-export FOX_TARGET_DEVICES="m55,m55xq"
+export FOX_TARGET_DEVICES="a35x"
 
 	# Let's see which are our build vars
     if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
