117a6c2f0b5bac1534cea540520a24c9fd3adc1e
Jamie Falconer <mrfluffyoven@gmail.com>
2025-12-04 11:16:20 +0000

    add api level and vndk

--- device.mk
+++ device.mk
@@ -10,13 +10,13 @@ LOCAL_PATH := device/samsung/a35x
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-# Recovery additional binaries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libc++_shared
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libc++_shared.so
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
